5ef4d1353599a97a8a29823bdceac4d3041ab0b3
nonydev <karldjansi123@gmail.com>
2026-03-14 13:54:05 +0000

    ...

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -69,8 +69,13 @@ export default function LoginPage() {
 
           <div className='flex justify-center text-gray-500'>
             <p className='text-sm '>
-              Ensure you confirm your email before signing in!
+              Ensure you confirm your email before signing in! 
             </p>
+
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5 text-red-500 ml-2">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+</svg>
+
           </div>
         {serverError && (
           <p className="text-red-500 text-sm bg-red-50 rounded-lg px-4 py-2">{serverError}</p>
